d696633204ed2c08a8ff1fdece7df6a693d9653f
Pedro <peterpendrag@gmail.com>
2020-11-03 14:57:10 +0100

    contact show create and index done

---
+++ src/Controller/Admin/ContactController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Contact;
+use App\Repository\ContactRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/contact")
+ */
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/", name="contact_index", methods={"GET"})
+     */
+    public function index(ContactRepository $contactRepository): Response
+    {
+        return $this->render('admin/contact/index.html.twig', [
+            'messages' => $contactRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="contact_show", methods={"GET"})
+     */
+    public function show(Contact $contact): Response
+    {
+        return $this->render('admin/contact/show.html.twig', [
+            'message' => $contact,
+        ]);
+    }
+}
--- src/Controller/Front/ContactController.php
+++ src/Controller/Front/ContactController.php
@@ -4,18 +4,24 @@ namespace App\Controller\Front;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index()
+    public function index(Request $request)
     {
         $contact = new Contact;
+        $contact->setUpdatedAt(new DateTime('now'));
+        $contact->setCreatedAt(new DateTime('now'));
         $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        // \dd($form->isSubmitted());
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($contact);
